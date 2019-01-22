6e7423e75a4cf83ed2a399941fc797382886f3b4
Jesús Bossa <me@jesusbossa.com>
2019-01-21 20:26:52 -0500

    [ADD] Project´s base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    CardsComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
