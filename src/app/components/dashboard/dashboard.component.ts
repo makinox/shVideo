ba0f45b35fba01f35d593856727f398c26af60c8
Jesús Bossa <me@jesusbossa.com>
2019-01-21 23:04:10 -0500

    [Add] Cards initialized

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -11,6 +11,7 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   data: Array<Object>
+  img: String = 'fw02ONlDhrYjTSZV8XO6hhU3ds3.jpg'
 
   async ngOnInit() {
     const data = await getMovies()
