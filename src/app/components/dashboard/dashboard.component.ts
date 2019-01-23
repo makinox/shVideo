c7185da6669f2f0cc66b00ca341af9f6fde06bca
Jesús Bossa <me@jesusbossa.com>
2019-01-22 23:44:38 -0500

    [ADD] Pagination events

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -30,17 +30,17 @@ export class DashboardComponent implements OnInit {
   }
 
   async getData() {
-    let data = {}
-    let genres = {}
+    let data: object = {}
+    let genres: object = {}
     if (window.location.pathname === "/series") {
-      data = await getSeries()
-      genres = await getMovieGenres()
+      data = await getSeries(this.returnPage(true))
+      genres = await getMovieGenres(this.returnPage(true))
     } else if (window.location.pathname === "/favoritos") {
-      data = await getMovies()
-      genres = await getMovieGenres()
+      data = await getMovies(this.returnPage(true))
+      genres = await getMovieGenres(this.returnPage(true))
     } else {
-      data = await getMovies()
-      genres = await getMovieGenres()
+      data = await getMovies(this.returnPage(true))
+      genres = await getMovieGenres(this.returnPage(true))
     }
     this.data = data['results']
     this.genres = genres['genres']
@@ -49,13 +49,13 @@ export class DashboardComponent implements OnInit {
   async handleChange(el: string) {
     if (el) {
       // console.log(el)
-      let data = {}
+      let data: object = {}
       if (window.location.pathname === "/series") {
-        data = await getSerieByName(el.trim(), this.year.value)
+        data = await getSerieByName(el.trim(), this.year.value, this.returnPage(true))
       } else if (window.location.pathname === "/favoritos") {
-        data = await getMovies()
+        data = await getMovies(this.returnPage(true))
       } else {
-        data = await getMovieByName(el.trim(), this.year.value)
+        data = await getMovieByName(el.trim(), this.year.value, this.returnPage(true))
       }
       this.data = data['results']
     } else {
@@ -64,15 +64,35 @@ export class DashboardComponent implements OnInit {
   }
 
   async handleFilters() {
-    let data = {}
+    let data: object = {}
     if (window.location.pathname === "/series") {
-      data = await getSeriesByFilters(this.year.value, this.genero.value)
+      data = await getSeriesByFilters(this.year.value, this.genero.value, this.returnPage(true))
     } else if (window.location.pathname === "/favoritos") {
-      data = await getMovies()
+      data = await getMovies(this.returnPage(true))
     } else {
-      data = await getMoviesByFilters(this.year.value, this.genero.value)
+      data = await getMoviesByFilters(this.year.value, this.genero.value, this.returnPage(true))
     }
     this.data = data['results']
+  }
+
+  handlePage(direction: string) {
+    let page: number = this.returnPage()
+    if (direction) {
+      page++
+      window.location.hash = `page=${page}`
+    } else {
+      if (page) page--
+      window.location.hash = `page=${page}`
+    }
+  }
+
+  returnPage(increase: boolean = false) {
+    let page: number = window.location.hash ? parseInt(window.location.hash.split('=')[1], 10) : 0
+    if (increase){
+      return page++
+    } else {
+      return page
+    }
   }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,45 +1,45 @@
-import {environment} from '../../environments/environment'
+import { environment } from '../../environments/environment'
 const base = 'https://api.themoviedb.org/3/'
 
-async function getMovies() {
-  const query = await fetch(`${base}movie/popular?api_key=${environment.key}`)
+async function getMovies(page: number = 0) {
+  const query = await fetch(`${base}movie/popular?api_key=${environment.key}&page=${page}`)
   const body = await query.json()
   return body
 }
 
-async function getSeries() {
-  const query = await fetch(`${base}tv/popular?api_key=${environment.key}`)
+async function getSeries(page: number = 0) {
+  const query = await fetch(`${base}tv/popular?api_key=${environment.key}&page=${page}`)
   const body = await query.json()
   console.log(body)
   return body
 }
 
-async function getMovieByName(name: string, search: string = '') {
-  const query = await fetch(`${base}search/movie?api_key=${environment.key}&query=${name}&year=${search}`)
+async function getMovieByName(name: string, search: string = '', page: number = 0) {
+  const query = await fetch(`${base}search/movie?api_key=${environment.key}&query=${name}&year=${search}&page=${page}`)
   const body = await query.json()
   return body
 }
 
-async function getSerieByName(name: string, search: string = '') {
-  const query = await fetch(`${base}search/tv?api_key=${environment.key}&query=${name}&year=${search}`)
+async function getSerieByName(name: string, search: string = '', page: number = 0) {
+  const query = await fetch(`${base}search/tv?api_key=${environment.key}&query=${name}&year=${search}&page=${page}`)
   const body = await query.json()
   return body
 }
 
-async function getMovieGenres() {
-  const query = await fetch(`${base}genre/movie/list?api_key=${environment.key}`)
+async function getMovieGenres(page: number = 0) {
+  const query = await fetch(`${base}genre/movie/list?api_key=${environment.key}&page=${page}`)
   const body = await query.json()
   return body
 }
 
-async function getMoviesByFilters(year: string = '', genre: string = '') {
-  const query = await fetch(`${base}discover/movie?api_key=${environment.key}&sort_by=popularity.desc&with_genres=${genre}&year=${year}`)
+async function getMoviesByFilters(year: string = '', genre: string = '', page: number = 0) {
+  const query = await fetch(`${base}discover/movie?api_key=${environment.key}&sort_by=popularity.desc&with_genres=${genre}&year=${year}&page=${page}`)
   const body = await query.json()
   return body
 }
 
-async function getSeriesByFilters(year: string = '', genre: string = '') {
-  const query = await fetch(`${base}discover/tv?api_key=${environment.key}&sort_by=popularity.desc&with_genres=${genre}&year=${year}`)
+async function getSeriesByFilters(year: string = '', genre: string = '', page: number = 0) {
+  const query = await fetch(`${base}discover/tv?api_key=${environment.key}&sort_by=popularity.desc&with_genres=${genre}&year=${year}&page=${page}`)
   const body = await query.json()
   return body
 }
